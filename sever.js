8ae852c8de16341c0cb1e6d6bdc72eeafa2df4e1
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-05-04 09:20:34 +0530

    set up the view engine and load all the assets

--- sever.js
+++ sever.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
 
 
-
+//creating an express instance
 const app = express();
 
 dotenv.config({path:'config.env'})
 const PORT = process.env.PORT || 8080
+
+//use to print log messages
+app.use(morgan('tiny'));
+app.use(bodyparser.urlencoded({extended:true}))
+
+//set the view engine
+app.set('view engine','ejs')
+//if the ejs files are in a seperate folder u have to use this as well
+//app.set('views',path.resolve(__dirname,'views/ejs'))
+
+//load assets
+app.set('/css',express.static(path.resolve(__dirname,'assets/css')))
+app.set('/img',express.static(path.resolve(__dirname,'assets/img')))
+app.set('/js',express.static(path.resolve(__dirname,'assets/js')))
+
+
+
 
 app.get('/',(req,res) =>{
     res.send('crud app')
