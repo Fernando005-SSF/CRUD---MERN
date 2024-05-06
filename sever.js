71810c12b70331b90150ad84c962db747d77fa66
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-05-06 13:28:11 +0530

    create update user and clear the dummy html code

--- sever.js
+++ sever.js
@@ -32,4 +32,12 @@ app.get('/',(req,res) =>{
     res.render('index')
 })
 
+app.get('/add-user',(req,res) =>{
+    res.render('add_user')
+})
+
+app.get('/update-user',(req,res) =>{
+    res.render('update_user')
+})
+
 app.listen(PORT,()=>{console.log(`You are listning to http://localhost:${PORT}`)})
