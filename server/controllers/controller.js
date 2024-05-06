1dd25b68747ebbc4ed7863402b9995c3f060c6c7
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-05-06 19:20:51 +0530

    create apis for user control

---
+++ server/controllers/controller.js
@@ -0,0 +1,25 @@
+const Userdb = require('../model/model');
+
+
+//create and save new user
+exports.create = (req,res)=>{
+
+}
+
+
+//retrieve and return all users/single user
+exports.find =(req,res)=>{
+
+}
+
+
+//update users\
+exports.update = (req,res)=>{
+
+}
+
+
+//delete user
+exports.delete = (req,res)=>{
+    
+}
