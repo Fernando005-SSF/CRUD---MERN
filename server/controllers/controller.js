3f968c103cbf0e902ca7a14ee6f6b25f13cbd874
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-05-07 13:23:13 +0530

    tested the api with postman

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -4,6 +4,32 @@ const Userdb = require('../model/model');
 //create and save new user
 exports.create = (req,res)=>{
 
+    //validate user
+    if(!req.body){
+        res.status(400).send({message :"content can not be empty"});
+        return;
+    }
+
+    //new user
+    const user =new Userdb({
+
+        name:req.body.name,
+        email:req.body.email,
+        gender:req.body.gender,
+        status : req.body.status
+
+    });
+
+    //save user inthe data base
+    user.save(user)
+                .then(data=>{
+                    res.send(data)
+                })
+                .catch(err=>{
+                    res.status(500).seend({
+                        message:err.message || "some error occured"
+                    });
+                });
 }
 
 
@@ -21,5 +47,5 @@ exports.update = (req,res)=>{
 
 //delete user
 exports.delete = (req,res)=>{
-    
+
 }
