b2c0bb7cbc5ba8e666863321840df07994b6d7ec
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-05-08 14:18:50 +0530

    completed the project with functoning the delete user

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,3 +24,28 @@ $('#update_user').submit(function(event){
         alert('Data updated successfully')
     })
 })
+
+//delete user
+
+if(window.location.pathname == '/'){
+
+    $ondelete = $('.table tbody td a.delete');
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+
+        
+    var request = {
+        "url" : `http://localhost:3000/api/users/${id}`,
+        "method":"DELETE"
+    }
+
+    if(confirm("Are you sure")){
+
+        $.ajax(request).done(function(response){
+            alert('Data deleted successfully')
+            location.reload();
+        })
+    }
+
+    })
+}
